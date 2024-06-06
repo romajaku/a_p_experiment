41656fb378d01f9a6c54e63bd77260081b394546
Jakub Romanowski <kuba.romanowski94@gmail.com>
2024-06-06 11:18:18 +0200

    error fix

--- main.py
+++ main.py
@@ -35,8 +35,7 @@ results_recall = list()
 @atexit.register
 def SaveResults():
     path = 'results'
-    isExist = os.path.exists(path)
-        if not isExist:
+    if not os.path.exists(path):
         os.makedirs(path)
     date_time_str = datetime.strftime('%Y_%m_%d_%H_%M_%S')
     with open(f'results/beh_{part_ID}_{part_Age}_{part_Gend}_{date_time_str}.csv', 'w', newline='') as csvfile:
